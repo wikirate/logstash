d49b5b217a8062ff0e0a14a61f0fd3e45d478225
Andrew Kostka <git@andrewkostka.com>
2024-10-19 20:28:04 +0000

    Beautify SQL queries

--- config/queries/companies.sql
+++ config/queries/companies.sql
@@ -1,19 +1,36 @@
-SELECT co.updated_at,
-  IF(co.created_at = co.updated_at, 'create', IF(co.trash=1, 'delete', 'update')) as action_type,
+SELECT
+  co.updated_at,
+  IF(
+    co.created_at = co.updated_at,
+    'create',
+    IF(co.trash = 1, 'delete', 'update')
+  ) AS action_type,
   co.id,
   co.name,
-  hq.db_content as headquarters,
-  GROUP_CONCAT(cids.db_content SEPARATOR ',') as corporate_identifiers
-FROM cards as co
-  LEFT JOIN cards as hq ON co.id = hq.left_id AND hq.right_id=:headquarters
-  LEFT JOIN cards as cids ON co.id=cids.left_id AND cids.right_id IN (SELECT id from cards where type_id=:company_identifier)
-WHERE co.type_id=:wikirate_company
+  hq.db_content AS headquarters,
+  GROUP_CONCAT(cids.db_content SEPARATOR ',') AS corporate_identifiers
+FROM
+  cards AS co
+  LEFT JOIN cards AS hq ON co.id = hq.left_id
+  AND hq.right_id = :headquarters
+  LEFT JOIN cards AS cids ON co.id = cids.left_id
+  AND cids.right_id IN (
+    SELECT
+      id
+    FROM
+      cards
+    WHERE
+      type_id = :company_identifier
+  )
+WHERE
+  co.type_id = :wikirate_company
   AND co.updated_at >= :sql_last_value
 GROUP BY
-    co.updated_at,
-    co.created_at,
-    co.trash,
-    co.id,
-    co.name,
-    hq.db_content
-ORDER BY co.updated_at;
+  co.updated_at,
+  co.created_at,
+  co.trash,
+  co.id,
+  co.name,
+  hq.db_content
+ORDER BY
+  co.updated_at;
